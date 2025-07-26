616dfc940e3e833bbbb39a27a3d0613997ba2472
vaibhavsarswat <22csaiml035@jssaten.ac.in>
2025-07-26 15:43:51 +0530

    Fix typo in access token payload: change 'fullName' to 'fullname'

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -60,7 +60,7 @@ userSchema.methods.generateAccessToken = function(){
             _id: this._id,
             email: this.email,
             username: this.username,
-            fullName: this.fullName,
+            fullName: this.fullname,
             role: this.role
         },
         process.env.ACCESS_TOKEN_SECRET,
